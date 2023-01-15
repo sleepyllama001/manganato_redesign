9d9a3727c450ceeac116cff6a12b8aabf6d642a8
eirik64 <70987628+eirik64@users.noreply.github.com>
2023-01-15 18:59:48 -0500

    small updates to Footter and images

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,7 @@
 import "./Footer.css";
 import React from "react";
 
+//
 export default function Footer() {
     return (
         <div className="footer_box">
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,12 +1,14 @@
 import "./Menu.css";
 import React from "react";
 
+// Creates the Nav bar! Includes the Manganato logo, Home,
+// Recently Updated, Trending, Login, Register, and Night Mode Buttons
 export default function Menu() {
     return (
         <div className="menu">
             <div className="logo">
                 <a className="horizontal_logo" href="../../public/index.html">
-                    <embed className="M" type="image/png" src="../../public/img/icons/manganato_logo.png"/>
+                    <embed className="M" type={"image/png"} src={"../../public/img/icons/manganato_logo.png"}/>
                         <div className="Manga"><h1>Manga</h1></div>
                         <div className="Nato"><h1>Nato</h1></div>
                 </a>
@@ -16,14 +18,14 @@ export default function Menu() {
                 <div className="border_box">
                     <ul className="horizontal">
                         <li><a id="Home" href="../../public/index.html">Home</a></li>
-                        <li><a id="Recent" href="recently_updated.html">Recently Updated</a></li>
-                        <li><a id="Trending" href="trending.html">Trending</a></li>
+                        <li><a id="Recent" href="../../public/recently_updated.html">Recently Updated</a></li>
+                        <li><a id="Trending" href="../../public/trending.html">Trending</a></li>
                     </ul>
                 </div>
             </div>
 
             <div className="border">
-                <a id="login" href="login_register.html">
+                <a id="login" href="../../public/login_register.html">
                     <div className="login_register">
                         <h2>Login</h2>
                     </div>
@@ -31,7 +33,7 @@ export default function Menu() {
             </div>
 
             <div className="border">
-                <a id="register" href="login_register.html">
+                <a id="register" href="../../public/login_register.html">
                     <div className="login_register">
                         <h2>Register</h2>
                     </div>
@@ -40,7 +42,7 @@ export default function Menu() {
 
             <div className="padding">
                 <div className="border_night">
-                    <a id="night-mode" href="night_mode.html">
+                    <a id="night-mode" href="../../public/index.html">
                         <div className="night_mode">
                             <embed className="moon" type="image/png" src="../../public/img/icons/crescent.png"/>
                                 <h2>Night Mode</h2>
